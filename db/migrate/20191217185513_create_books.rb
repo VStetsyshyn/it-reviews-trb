857ea5062404fd97e4c7f97ad5180b80c645d5a2
pyrin andrii <pyrin.andrii@gmail.com>
2019-12-17 21:52:10 +0200

    Add table and model

---
+++ db/migrate/20191217185513_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :author, null: false
+      t.string :title, null: false
+      t.string :publisher
+      t.string :categories
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_213526) do
+ActiveRecord::Schema.define(version: 2019_12_17_185513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "author", null: false
+    t.string "title", null: false
+    t.string "publisher"
+    t.string "categories"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
