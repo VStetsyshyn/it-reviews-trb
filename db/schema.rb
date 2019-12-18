0fe1d131381b52ebfd72d566113b0836ac7b29f4
pyrin andrii <pyrin.andrii@gmail.com>
2019-12-18 23:59:23 +0200

    Add Category model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_185513) do
+ActiveRecord::Schema.define(version: 2019_12_18_204504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,13 @@ ActiveRecord::Schema.define(version: 2019_12_17_185513) do
     t.string "author", null: false
     t.string "title", null: false
     t.string "publisher"
-    t.string "categories"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
