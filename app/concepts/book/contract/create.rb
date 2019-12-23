8d684ddd8a46909adde83f4a0ec896d33457b605
pyrin andrii <pyrin.andrii@gmail.com>
2019-12-23 22:24:41 +0200

    Add validation for unique pair title and author of book

--- app/concepts/book/contract/create.rb
+++ app/concepts/book/contract/create.rb
@@ -8,9 +8,25 @@ class Book < ApplicationRecord
       property :publisher
       property :category
 
-      validation do
-        required(:author).filled
+      validation :default do
         required(:title).filled
+        required(:author).filled
+      end
+
+      validation :unique_title_for_author, if: :default, with: {form: true} do
+        configure do
+          config.messages_file = './config/errors.yml'
+
+          def title
+            form.input_params[:title]
+          end
+
+          def unique_title_for_author?(author)
+            !Book.where(author: author, title: title).exists?
+          end
+        end
+
+        required(:author).filled(:unique_title_for_author?)
       end
     end
   end
